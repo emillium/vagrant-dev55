ccced9c78c42ba2c397ac8944555d115f5ed28e5
Tim Colbert <tim.colbert@temando.com>
2015-09-10 15:22:31 +1000

    Fix to install packages

--- provision/shellScripts/install-packages.sh
+++ provision/shellScripts/install-packages.sh
@@ -3,8 +3,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 PACKAGES=$(echo "$1")
 
-type ${PACKAGES} >/dev/null 2>&1 || {
-  echo "Installing ${PACKAGES}"
-  apt-get install -y ${PACKAGES} >/dev/null
-  echo "Installed ${PACKAGES} packages"
+type '${PACKAGES}' >/dev/null 2>&1 || {
+ echo "Installing ${PACKAGES}"
+ apt-get install -y "${PACKAGES}" >/dev/null
+ echo "Installed ${PACKAGES} packages"
 }
