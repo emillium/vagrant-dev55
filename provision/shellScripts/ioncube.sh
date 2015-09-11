5c23524edd4b9e6742d5191e2dd6d6b19ed7eb3b
Jason Wijegooneratne <jason.wijegooneratne@temando.com>
2015-09-11 14:25:10 +1000

    updated setup proceedure for API project

--- provision/shellScripts/ioncube.sh
+++ provision/shellScripts/ioncube.sh
@@ -1,8 +1,5 @@
 # Ioncube
-pushd /root/
 wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar zxvf ioncube_loaders_lin_x86-64.tar.gz
 sudo cp ioncube/ioncube_loader_lin_5.5.so /usr/lib/php5/20121212/
 sudo sed -i '3i\'"zend_extension = /usr/lib/php5/20121212/ioncube_loader_lin_5.5.so" /etc/php5/apache2/php.ini
-# echo "zend_extension = /usr/lib/php5/20121212/ioncube_loader_lin_5.5.so" | sudo tee -a /etc/php5/apache2/php.ini
-popd
--- provision/shellScripts/newrelic.sh
+++ provision/shellScripts/newrelic.sh
@@ -1,5 +1,5 @@
 # Trust newRelic repo and add it to source.list
 wget -qO - https://download.newrelic.com/548C16BF.gpg | sudo apt-key add -
 echo "deb http://apt.newrelic.com/debian/ newrelic non-free" | sudo tee -a /etc/apt/sources.list.d/newrelic.list
-apt-get update
-apt-get -y install newrelic-php5
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install newrelic-php5
