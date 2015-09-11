354638eaa4a70d7b95438179eaae23964b5ab088
Jason Wijegooneratne <jason.wijegooneratne@temando.com>
2015-09-10 22:25:53 -0400

    update required pems

--- provision/shellScripts/ioncube.sh
+++ provision/shellScripts/ioncube.sh
@@ -2,7 +2,7 @@
 pushd /root/
 wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar zxvf ioncube_loaders_lin_x86-64.tar.gz
-cp ioncube/ioncube_loader_lin_5.5.so /usr/lib/php5/20121212/
-sed -i '1i\'"zend_extension = /usr/lib/php5/20121212/ioncube_loader_lin_5.5.so" /etc/php5/apache2/php.ini
+sudo cp ioncube/ioncube_loader_lin_5.5.so /usr/lib/php5/20121212/
+sudo sed -i '1i\'"zend_extension = /usr/lib/php5/20121212/ioncube_loader_lin_5.5.so" /etc/php5/apache2/php.ini
 # echo "zend_extension = /usr/lib/php5/20121212/ioncube_loader_lin_5.5.so" | sudo tee -a /etc/php5/apache2/php.ini
 popd
